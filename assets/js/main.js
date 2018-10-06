045642372aa6fb92199268376b29249f6c2e6284
YasinEhsan <yasinehsan11@gmail.com>
2018-10-06 03:41:46 -0500

    merge success

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
+
+//date picker
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, options);
   });
 
+//form dropdown
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
+  });
+  
+//Custom ID
+  var input = document.getElementById("myInput");
+  input.addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+          document.getElementById("myBtn").click();
+      }
   });
