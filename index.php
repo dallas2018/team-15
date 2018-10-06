6945483a1d97d12ed4371b66a4497099667719c5
Samad K Virani <virani.samad@gmail.com>
2018-10-06 05:20:19 -0500

    fixing test form

--- index.php
+++ index.php
@@ -7,12 +7,18 @@ $db_name = $ini['db_name'];
 $db_user = $ini['db_user'];
 $db_pass = $ini['db_pass'];
 
+$mysqli = new mysqli_connect("localhost", $db_user, $db_pass, $db_name);
+
+if(!$mysqli) {
+    echo "THERE WAS AN ERROR CONNECTING";
+}
+
 echo $db_name . " " . $db_user . " " . $db_pass . " ";
 
 echo "<html>";
 echo "<body>";
 
-echo "<form id='test' action='post' action='postData.php'>";
+echo "<form id='test' method='post' action='postData.php'>";
     echo "Name: <input type='text' name='name'><br>";
     echo "SSN: <input type='text' name='ssn'><br>";
     echo "<input type='submit'>";
